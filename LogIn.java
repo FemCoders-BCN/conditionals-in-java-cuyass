1017273ff9214232454b027137b6389ba5f449c7
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-02-21 12:09:26 +0100

    todo hecho!

--- LogIn.java
+++ LogIn.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class LogIn {
     public static void main(String[] args) {
         //Escribe un programa que pida al usuario ingresar un nombre de usuario y una constraseña, si son iguales a los datos dados imprimirá en terminal: Acceso concedido, si no son iguales imprimirá en terminal: Nombre de usuario o contraseña incorrecta.
         //Averigua como hacer para poder ingresar el dato justo al lado de lo que termine la pregunta, es decir, que no haga un salto de línea.
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, introduce tu nombre de usuario: ");
+        String name = scanner.nextLine();
+        System.out.print("Por favor, introduce tu contraseña: ");
+        String secret = scanner.nextLine();
+
+        String username = "marimpuri";
+        String password = "Avemaria123!";
+        
+        if(username.equals(name) && password.equals(secret)) {
+            System.out.println("Acceso concedido");
+        } else {
+            System.out.println("Nombre de usuario o contraseña incorrecta");
+        }
+
+        scanner.close();
     }
 }
--- Quiz.java
+++ Quiz.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Quiz {
     public static void main(String[] args) {
         //Escribe un programa que sea un pequeño juego trivial de acertar preguntas
@@ -9,6 +11,67 @@ public class Quiz {
         //De lo contrario imprime: Buena suerte la próxima vez
         //Las opciones a, b, c y d de cada pregunta, deberán dejar en terminal un espacio de sangría, averigua como hacerlo en java
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Te damos la bienvenida a la mini trivia de F5.\nEl juego consta de 4 preguntas de mútliple respuesta.\nPara responder, solo tienes que escribir la letra de la respuesta correcta.\n¿Empezamos?");
+
+        int score = 0;
+
+        System.out.println("Pregunta 1\n¿Quién fue la hacker que escribió en 1994 'Hacking the Wetware'?");
+        System.out.println("\ta. Ada Lovelace\n\tb. Jude Milhon\n\tc. Martha Ackelsberg\n\td. Jane Jacobs");
+
+        String q1 = scanner.nextLine();
+        String q1Ok = "b";
+
+        if(q1.equals(q1Ok)) {
+            score += 5;
+        } else {
+            score += 0;
+        }
+
+        System.out.println("Pregunta 2\n¿Quién es Fuencisla Clemares?");
+        System.out.println("\ta. La directora general de IBM Europa, Oriente Medio y África\n\tb. La vicepresidenta de Microsoft Western Europe\n\tc. La directora general de Google en España y Portugal\n\td. Fundadora y CEO de Marsi Bionics");
+
+        String q2 = scanner.nextLine();
+        String q2Ok = "c";
+
+        if(q2.equals(q2Ok)) {
+            score += 5;
+        } else {
+            score += 0;
+        }
+
+        System.out.println("Pregunta 3\n¿Quién fue Hedwig Eva Maria Kiesler, conocida como Hedy Lamarr? (tienes que escoger dos respuestas correctas)");
+        System.out.println("\ta. Primera programadora de software\n\tb. Una actriz de cine\n\tc. Inventora del procesador de datos\n\td. Inventora del sistema de comunicaciones de salto de frecuencia, base del wifi, bluetooth y GPS");
+
+        String q3 = scanner.nextLine();
+        String q3Ok = "b y d";
+
+        if(q3.equals(q3Ok)) {
+            score += 5;
+        } else {
+            score += 0;
+        }
+
+        System.out.println("Pregunta 4\n¿Cómo se llamaban las principales 'mujeres ocultas' de la NASA, encargadas de los cálculos para que la misión del Apolo 11 concluyera con éxito?");
+        System.out.println("\ta. Katherine Johnson, Dorothy Vaughan y Mary Jackson\n\tb. Jessica Miller, Emily Johnson, Sarah Davis\n\tc. Bell Hooks, Toni Morrison y Alice Walker\n\td. Emmeline Pankhurst, Millicent Fawcett y Harriet Harman");
+
+        String q4 = scanner.nextLine();
+        String q4Ok = "a";
+
+        if(q4.equals(q4Ok)) {
+            score += 5;
+        } else {
+            score += 0;
+        }
+
+        if(score >= 15) {
+            System.out.println("Tú sí que sabes mucho");
+        } else if((score < 15) && (score >= 5)) {
+            System.out.println("No lo has hecho mal");
+        } else if(score < 5) {
+            System.out.println("Buena suerte la próxima vez");
+        }
         //Preguntas:
         /* 1. ¿Quién fue la hacker que escribió en 1994 Hacking the Wetware?
                 a. Ada Lovelace
@@ -37,6 +100,7 @@ public class Quiz {
 
         //Respuestas: 1. b / 2. c / 3. b y d / 4. a
 
+        scanner.close();
     }
     
 }
